dc77e0998f538d7d74c493f887e21b2337387d58
Dominik Brzóska <d.brzoska@telemedi.com>
2023-02-15 13:35:10 +0100

    feat: add symfony project schema

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # e-signature
+{"username":"user","password":"X7VmcLBscqu9XvY"}
--- app/config/bundles.php
+++ app/config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Prugala\RequestDto\RequestDtoBundle::class => ['all' => true],
 ];
