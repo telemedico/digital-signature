95cd3d9ad4bbd1484f36e8f000ed9e06ec93517b
Dominik Brzóska <d.brzoska@telemedi.com>
2023-02-21 08:28:13 +0100

    feat(LESS-3592): add postgres and doctrine

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -7,4 +7,7 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Prugala\RequestDto\RequestDtoBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
